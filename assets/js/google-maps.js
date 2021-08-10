95a88b67cbab55066d6fcfcc1c912314d2bf7b41
SirJackSparow <maulanabima785@gmail.com>
2021-08-10 11:13:37 +0700

    message

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 31.2242387, lng: 29.8848462};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
